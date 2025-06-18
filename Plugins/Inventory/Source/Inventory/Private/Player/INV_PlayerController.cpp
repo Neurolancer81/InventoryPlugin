5f654801f1a8d7bd3d79f3c6794e17ac356718d0
Neurolancer81 <87434607+Neurolancer81@users.noreply.github.com>
2025-06-18 14:41:32 -0400

    Added Tracing

---
+++ Plugins/Inventory/Source/Inventory/Private/Player/INV_PlayerController.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/INV_PlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Widgets/HUD/INV_HUDWidget.h"
+
+
+void AINV_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem =
+		ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+
+	if(IsValid(Subsystem))
+	{
+		for (UInputMappingContext* CurrentContext : DefaultIMCs)
+		{
+			Subsystem->AddMappingContext(CurrentContext, 0);
+		}
+	}
+
+	CreateHUDWidget();
+}
+
+void AINV_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(PrimaryInteractAction,
+		ETriggerEvent::Started,
+		this,
+		&AINV_PlayerController::PrimaryInteract
+		);
+}
+
+void AINV_PlayerController::PrimaryInteract()
+{
+	UE_LOG(LogTemp, Display, TEXT("Primary Interact"));
+}
+
+void AINV_PlayerController::CreateHUDWidget()
+{
+	if (!IsLocalController()) return;
+
+	HUDWidget = CreateWidget<UINV_HUDWidget>(this, HUDWidgetClass);
+	if (IsValid(HUDWidget))
+	{
+		HUDWidget->AddToViewport();
+	}
+}
---
+++ Plugins/Inventory/Source/Inventory/Public/Player/INV_PlayerController.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "INV_PlayerController.generated.h"
+
+class UINV_HUDWidget;
+class UInputAction;
+class UInputMappingContext;
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API AINV_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+private:
+	
+	void PrimaryInteract();
+	void CreateHUDWidget();
+	
+	UPROPERTY(EditDefaultsOnly, Category="Inventory")
+	TArray<TObjectPtr<UInputMappingContext>> DefaultIMCs;
+
+	UPROPERTY(EditDefaultsOnly, Category="Inventory")
+	TObjectPtr<UInputAction> PrimaryInteractAction;
+
+	UPROPERTY(EditDefaultsOnly, Category="Inventory")
+	TSubclassOf<UINV_HUDWidget> HUDWidgetClass;
+
+	UPROPERTY()
+	TObjectPtr<UINV_HUDWidget> HUDWidget;
+};
